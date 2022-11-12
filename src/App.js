d1d3b3cb727fc25dfa255e49add6ae62fa33ae5e
Abhijeet-Trojan <trojanabhijeet@gmail.com>
2022-11-13 01:36:03 +0530

    added invoices page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
 import Team from './scenes/team';
-// import Invoices from './scenes/invoices';
 import Contacts from './scenes/contacts';
+import Invoices from './scenes/invoices';
 // import Bar from './scenes/bar';
 // import Form from './scenes/form';
 // import Line from './scenes/line';
@@ -32,8 +32,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Dashboard />} />
             <Route path='/team' element={<Team />} />
-            {/* <Route path='/invoices' element={<Invoices />} /> */}
             <Route path='/contacts' element={<Contacts />} />
+            <Route path='/invoices' element={<Invoices />} />
             {/* <Route path='/bar' element={<Bar/>} /> */}
             {/* <Route path='/form' element={<Form />} /> */}
             {/* <Route path='/line' element={<Line />} /> */}
