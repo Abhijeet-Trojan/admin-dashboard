eddacd47c8923ebc6310ef8cdee7f0acea3e0d5a
Abhijeet-Trojan <trojanabhijeet@gmail.com>
2022-11-14 14:18:56 +0530

    Updates

---
+++ precache-manifest.6df0063274435e4ac0e5a30c1ba3dcfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92a4f946bde97612c44d",
+    "url": "/admin-dashboard/static/css/main.fc687acf.chunk.css"
+  },
+  {
+    "revision": "92a4f946bde97612c44d",
+    "url": "/admin-dashboard/static/js/main.92a4f946.chunk.js"
+  },
+  {
+    "revision": "9794842104c33f4fcaaf",
+    "url": "/admin-dashboard/static/css/1.f89900b0.chunk.css"
+  },
+  {
+    "revision": "9794842104c33f4fcaaf",
+    "url": "/admin-dashboard/static/js/1.97948421.chunk.js"
+  },
+  {
+    "revision": "1c7b39fc7ea0490cf413",
+    "url": "/admin-dashboard/static/js/runtime~main.1c7b39fc.js"
+  },
+  {
+    "revision": "22ce19f6b58e426867c438b92d5911cb",
+    "url": "/admin-dashboard/index.html"
+  }
+];
